d995cb4cc0ea2abf8bc1eb5b7097e6c4ef4c345c
Vidushi Sewmika <116452885+ViduSew@users.noreply.github.com>
2025-05-04 13:52:10 +0530

    Add files via upload

---
+++ TUT/tute01ii.java
@@ -0,0 +1,7 @@
+public class tute01ii {
+
+	public static void main(String[] args) {
+		System.out.println("first line");
+		System.out.println("This is  the second line");
+	}
+}
---
+++ TUT/tute01iii.java
@@ -0,0 +1,6 @@
+public class tute01iii {
+
+	public static void main(String[] args) {
+		System.out.println("Hello! Welcome to SLIIT");
+	}
+}
